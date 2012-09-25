aa117d24e61dec435b3f1b0fefbbef167209ce17
vishal kajjam <vishal.kvn@gmail.com>
2012-09-25 07:22:32 -0400

    cleaned up the code

--- search.js
+++ search.js
@@ -3,63 +3,11 @@ $(document).ready( function(){
 	var qstring;
 	var page = 1;
     var per_page = 9;
-    // var loading = 0;
-
- //    //the status of overlay box
-	// var isOpen = false;
-	// //function to display the box
-	// function showOverlayBox() {
-	// 	//if box is not set to open then don't do anything
-	// 	if( isOpen == false ) return;
-	// 	// set the properties of the overlay box, the left and top positions
-	// 	$('.overlayBox').css({
-	// 		display:'block',
-	// 		left:( $(window).width() - $('.overlayBox').width() )/2,
-	// 		top:( $(document).height() - $('.overlayBox').height() )/2 -20,
-	// 		position:'absolute'
-	// 	});
-	// 	// set the window background for the overlay. i.e the body becomes darker
-	// 	$('.bgCover').css({
-	// 		display:'block',
-	// 		width: $(window).width(),
-	// 		height:$(window).height(),
-	// 	});
-	// }
-	// function doOverlayOpen() {
-	// 	//set status to open
-	// 	isOpen = true;
-	// 	showOverlayBox();
-	// 	$('.bgCover').css({opacity:0}).animate( {opacity:0.5, backgroundColor:'#000'} );
-	// 	// dont follow the link : so return false.
-	// 	return false;
-	// }
-	// function doOverlayClose() {
-	// 	//set status to closed
-	// 	isOpen = false;
-	// 	$('.overlayBox').css( 'display', 'none' );
-	// 	// now animate the background to fade out to opacity 0
-	// 	// and then hide it after the animation is complete.
-	// 	$('.bgCover').animate( {opacity:0}, null, null, function() { $(this).hide(); } );
-	// }
-	// // if window is resized then reposition the overlay box
-	// $(window).bind('resize',showOverlayBox);
-
-
-	// // activate when the link with class launchLink is clicked
-	// // $('a.launchLink').click( doOverlayOpen );
-	// $("#images").delegate(".photo", "click", doOverlayOpen ); ***************
-
-
-	// // close it when closeLink is clicked
-	// $('a.closeLink').click( doOverlayClose );
-
-	    //select all the a tag with name equal to modal
+ 
     $("#images").delegate(".photo", "click", function(e) {
         //Cancel the link behavior
         e.preventDefault();
 
-        //Get the A tag
-        // var id = $(this).attr('href');
         var id = $("#dialog");
 
         //Get the screen height and width
@@ -134,16 +82,10 @@ $(document).ready( function(){
 
     $(window).scroll($.debounce(50 , function(){
 
-    	// if (loading == 0){
     		if  ($(window).scrollTop() == $(document).height() - $(window).height()){
-    		   // loading = 1;
 	           page = page + 1;
 	           search_flickr(qstring);
-	           // loading = 0;
-
-
 	        }
-    	// }
 
 		}
 	));
